3f4a297977f2591d635fd41e73b46691ac463447
Alexandre Bezroutchko <abb@gremwell.com>
2018-01-29 12:23:04 +0100

    bugfixes

--- geth-net24601.sh
+++ geth-net24601.sh
@@ -1,14 +1,13 @@
 #!/bin/sh -x
 
 NAME=geth-net24601
-#PORT=30303
-#RPCPORT=8545
+RPCPORT=8545
 
 BASEDIR=`dirname $0`
 DATADIR="$HOME/data/$NAME"
 
 DOCKER_RUN_INIT="docker run --rm  -v $DATADIR:/root --name $NAME ethereum/client-go:stable --networkid 24601"
-DOCKER_RUN="docker run -d --privileged --net mynet --ip 172.18.0.18 -v $DATADIR:/root --name $NAME ethereum/client-go:stable --networkid 24601"
+DOCKER_RUN="docker run -d --privileged --net mynet --ip 172.18.0.18 -p 127.0.0.1:$RPCPORT:8545/tcp -v $DATADIR:/root --name $NAME ethereum/client-go:stable --networkid 24601"
 
 case $1 in
         restart)
@@ -28,16 +27,20 @@ case $1 in
                 ;;      
 
 	init)
-		mkdir -p $DATADIR && cp $BASEDIR/geth-genesis.json $BASEDIR/geth-initaccounts.js $DATADIR/
+		mkdir -p $DATADIR
+		cp $BASEDIR/geth-genesis.json $BASEDIR/geth-initaccounts.js $BASEDIR/geth-fundaccounts.js $DATADIR/
 		$DOCKER_RUN_INIT init /root/geth-genesis.json
 		$DOCKER_RUN_INIT --exec "loadScript('/root/geth-initaccounts.js')" console
                 ;;
 	run)
 		$DOCKER_RUN --mine --minerthreads 1 --unlock 0,1,2,3,4,5 --password /dev/null \
  			--nodiscover --netrestrict 172.18.0.0/24 --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --verbosity 5
-		# -p $PORT:30303/tcp -p $PORT:30303/udp \ -p 127.0.0.1:$RPCPORT:8545/tcp \
 		docker exec $NAME hwclock -s
 		;;
+
+        fund)
+		docker exec $NAME geth --exec "loadScript('/root/geth-fundaccounts.js')" attach
+                ;;
 
 	kill)
 		docker kill $NAME
@@ -51,12 +54,16 @@ case $1 in
 		docker rm $NAME
 		;;
 
+	wipedb)
+		rm -rf $DATADIR/.ethereum
+		;;
+
 	wipe)
 		rm -rf $DATADIR
 		;;
 
 	attach)
-		docker exec -it $NAME geth attach /root/.ethereum/geth.ipc
+		docker exec -it $NAME geth attach
 		;;
 
 	logs)
