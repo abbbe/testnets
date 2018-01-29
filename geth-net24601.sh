82352afd60ad243397b3c48b57531976a1be5eb8
Alexandre Bezroutchko <abb@gremwell.com>
2018-01-29 11:58:18 +0100

    they sync

--- geth-net24601.sh
+++ geth-net24601.sh
@@ -7,8 +7,8 @@ NAME=geth-net24601
 BASEDIR=`dirname $0`
 DATADIR="$HOME/data/$NAME"
 
-DOCKER_RUN_INIT="docker run --rm  -v $DATADIR:/root/.ethereum --name $NAME ethereum/client-go:stable --networkid 24601"
-DOCKER_RUN="docker run -d --net mynet --ip 172.18.0.18 -v "$DATADIR:/root/.ethereum" --name $NAME ethereum/client-go:stable --networkid 24601"
+DOCKER_RUN_INIT="docker run --rm  -v $DATADIR:/root --name $NAME ethereum/client-go:stable --networkid 24601"
+DOCKER_RUN="docker run -d --privileged --net mynet --ip 172.18.0.18 -v $DATADIR:/root --name $NAME ethereum/client-go:stable --networkid 24601"
 
 case $1 in
         restart)
@@ -29,13 +29,14 @@ case $1 in
 
 	init)
 		mkdir -p $DATADIR && cp $BASEDIR/geth-genesis.json $BASEDIR/geth-initaccounts.js $DATADIR/
-		$DOCKER_RUN_INIT init /root/.ethereum/geth-genesis.json
-		$DOCKER_RUN_INIT --exec "loadScript('/root/.ethereum/geth-initaccounts.js')" console
+		$DOCKER_RUN_INIT init /root/geth-genesis.json
+		$DOCKER_RUN_INIT --exec "loadScript('/root/geth-initaccounts.js')" console
                 ;;
 	run)
 		$DOCKER_RUN --mine --minerthreads 1 --unlock 0,1,2,3,4,5 --password /dev/null \
- 			--nodiscover --netrestrict 172.18.0.0/24 --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*"
+ 			--nodiscover --netrestrict 172.18.0.0/24 --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --verbosity 5
 		# -p $PORT:30303/tcp -p $PORT:30303/udp \ -p 127.0.0.1:$RPCPORT:8545/tcp \
+		docker exec $NAME hwclock -s
 		;;
 
 	kill)
--- parity-net24601.sh
+++ parity-net24601.sh
@@ -1,29 +1,42 @@
 #!/bin/sh -x
 
 NAME=parity-net24601
+BASEDIR=`dirname $0`
+DATADIR=$HOME/data/$NAME
 
 PORT=34343
 UIPORT=48180
 RPCPORT=48545
 WSPORT=48546
 PORTSHIFT=40000
-DATADIR=$HOME/data/$NAME
+
 
 case $1 in
         restart)
                 $0 kill
                 $0 rm
-                $0 run  
-                ;;      
+                $0 run
+                $0 logs
+                ;;
+
+        reinit)
+                $0 kill
+                $0 rm
+                $0 wipe
+                $0 init
+                $0 run
+                $0 logs
+                ;;
 
 	run)
 		mkdir -p $DATADIR
-		cp parity-net24601.json $DATADIR
-		docker run -d --net mynet --ip 172.18.0.17 --name $NAME -p 127.0.0.1:$UIPORT:$UIPORT -p 127.0.0.1:$RPCPORT:$RPCPORT -p 127.0.0.1:$WSPORT:$WSPORT \
-			-v $DATADIR:/root/.local/share/io.parity.ethereum \
-			parity/parity:stable-release --ui-interface all --jsonrpc-interface all --ws-interface all --ws-origins http://localhost:$UIPORT \
-				--ports-shift $PORTSHIFT --chain=/root/.local/share/io.parity.ethereum/parity-net24601.json --tracing=on --fat-db=on --pruning=archive -ldiscovery,tpc=trace
+		cp $BASEDIR/parity-net24601.json $DATADIR/
+		docker run -d --privileged --net mynet --ip 172.18.0.17 --name $NAME -p 127.0.0.1:$UIPORT:$UIPORT -p 127.0.0.1:$RPCPORT:$RPCPORT -p 127.0.0.1:$WSPORT:$WSPORT \
+			-v $DATADIR:/root \
+			parity/parity:stable-release --ui-interface all --jsonrpc-interface all --ws-interface all --ws-origins http://localhost:$UIPORT --allow-ips private \
+				--ports-shift $PORTSHIFT --chain=/root/parity-net24601.json --tracing=on --fat-db=on --pruning=archive -ldiscovery,tpc=trace
 		# -p $PORT:30303/tcp -p $PORT:30303/udp \
+		docker exec $NAME hwclock -s
 		;;
 
 	kill)
